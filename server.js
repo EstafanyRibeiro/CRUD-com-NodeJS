f9b289fe9727cc6ad63b29739a4ed636f627e2ed
Estafany Ribeiro <69740553+EstafanyRibeiro@users.noreply.github.com>
2022-09-18 17:25:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,34 +5,6 @@ const express = require('express');
 
 const app = express ();
 
-/*const pg = require("pg");
-
-const config = {
-    host: 'db-crud.postgres.database.azure.com',
-    user: 'estafanyribeiro',
-    password: 'Vondy061@',
-    database: 'db-crud',
-    port: 5432,
-    ssl: false
-};
-
-const client = new pg.Client(config);
-
-client.connect(err => {
-    console.log(req.body);
-    else {
-        queryDatabase();
-});
-
-function queryDatabase() {
-    const query = `
-    CREATE TABLE dados_in (id serial PRIMARY KEY, name VARCHAR(60), age INTEGER, adress VARCHAR(90), email VARCHAR(40), fone INTEGER, graduacao VARCHAR (150), cursos VARCHAR (150), exp_profissional VARCHAR(150));
-    INSERT INTO dados_in 
-    `
-}*/
-
-
-
 app.listen(3000, function() {
     console.log('server running on port 3000')
 });
